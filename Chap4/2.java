640d85138827d87b65c68a36d905cb9f0266165f
RunaNam <nmj5012@naver.com>
2020-03-06 15:52:45 +0900

    0305

---
+++ Chap4/2.java
@@ -0,0 +1,13 @@
+class FlowEx22 {
+    public static void main (String[] args){
+        int [] arr = {10,20, 30, 40, 50};
+        int sum =0;
+        for (int i= 0; i<arr.length; i++){
+            System.out.printf("%d ", arr[i]);
+        }
+        for (int tmp: arr){
+            sum += tmp;
+        }
+        System.out.println(sum);
+    }
+}
