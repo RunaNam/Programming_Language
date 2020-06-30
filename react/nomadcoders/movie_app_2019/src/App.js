4d25a8b14f77f738b79c420f6fd0a18bdafaddac
2018029416 <nmj5012@naver.com>
2020-06-30 22:25:02 +0900

    0630_chap3

--- react/nomadcoders/movie_app_2019/src/App.js
+++ react/nomadcoders/movie_app_2019/src/App.js
@@ -1,42 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const foodLike = [
-  {
-    name: "Kimchi",
-    rating: 6,
-  },
-  {
-    name: "ramen",
-    rating: 4,
-  },
-  {
-    name: "rice",
-    rating: 3,
-  },
-];
-
-function renderFood(dish) {
-  return <Food name={dish.name} rating={dish.rating} />;
-}
-
-Food.PropTypes = {};
-
-function Food({ name, rating }) {
-  return (
-    <div>
-      <h3> I like {name}</h3>
-      <h4>{rating}/5</h4>
-    </div>
-  );
-}
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      {foodLike.map(renderFood)}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
+  };
+  minus = () => {
+    this.setState((current) => ({
+      count: current.count - 1,
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <h1> Im a class {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
